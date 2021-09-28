115dc53b8adc7f0ce4d29d77da9492532b6c77ee
01Clarian <clarionnorth@gmail.com>
2021-09-27 23:45:05 -0400

    Updates

---
+++ precache-manifest.30feaeeb0d87e524df7e5bafea355bf3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e786209bf9386cb76ecf7e8dadab1a6",
+    "url": "/whaleshark/index.html"
+  },
+  {
+    "revision": "1c0d983f56792414cd41",
+    "url": "/whaleshark/static/css/main.1fe6a57a.chunk.css"
+  },
+  {
+    "revision": "5ec92116b7d1dcf3fb98",
+    "url": "/whaleshark/static/js/2.8eb2e461.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/whaleshark/static/js/2.8eb2e461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0d983f56792414cd41",
+    "url": "/whaleshark/static/js/main.33bbc12f.chunk.js"
+  },
+  {
+    "revision": "da7ccbe25b49ec328b15",
+    "url": "/whaleshark/static/js/runtime-main.1371bd90.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/whaleshark/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "2dd0381125ec17c86cdd9fdcd73957e4",
+    "url": "/whaleshark/static/media/features-split-image-01.2dd03811.png"
+  },
+  {
+    "revision": "96dc56cbeab8edcfc2c194e5cce9a0e4",
+    "url": "/whaleshark/static/media/features-split-image-02.96dc56cb.png"
+  },
+  {
+    "revision": "f9c218d0134418881b1d8fb3b8de9953",
+    "url": "/whaleshark/static/media/features-split-image-03.f9c218d0.png"
+  },
+  {
+    "revision": "09b755d6dd89be478e5382dd6f9b8ad7",
+    "url": "/whaleshark/static/media/firstedition.09b755d6.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/whaleshark/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/whaleshark/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "da5dd51505b9673919518b373a43137c",
+    "url": "/whaleshark/static/media/image-3.da5dd515.png"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/whaleshark/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a982d89fec8f2b4169b824703c162cb5",
+    "url": "/whaleshark/static/media/ultradragonhash.a982d89f.png"
+  },
+  {
+    "revision": "4ff595aa961ab60d8f04810504132f7f",
+    "url": "/whaleshark/static/media/videosample.4ff595aa.png"
+  },
+  {
+    "revision": "df244e432989212291ca72b1004ef620",
+    "url": "/whaleshark/static/media/whalesharktitle.df244e43.png"
+  }
+]);
